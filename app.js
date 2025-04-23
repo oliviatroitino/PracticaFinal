f1ac655b427bbd40ed32e5ced484fe3b54a2af15
oliviatroitino <olivia@cygnuskraft.es>
2025-04-23 09:35:18 +0200

    add routers and routes

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const express = require("express");
 const cors = require("cors");
 
 const dbConnect = require('./config/mongo');
-// const usersRouter = require("./routes/user.js");
-// const authRouter = require("./routes/auth.js");
+const usersRouter = require("./routes/user.js");
+const authRouter = require("./routes/auth.js");
+const clientRouter = require('./routes/client.js');
+const pdfRouter = require('./routes/pdf.js')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use('/users', usersRouter);
-// app.use('/auth', authRouter)
+app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/clients', clientRouter);
+app.use('/pdf', pdfRouter);
 
 dbConnect()
 
